4d48621dbc086c54eadf149b2078f0d525ea19fc
Ray <sewellray11@googlemail.com>
2021-10-04 15:31:05 +0100

    created APICall class, APOD model + call method

---
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/APICall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using RestSharp;
+using Newtonsoft.Json;
+using MarsRoverBestGroup3._0.Models;
+
+namespace MarsRoverBestGroup3._0
+{
+    public class APICall
+    {
+        /* To use on a webpage add the block of code below :)
+         * 
+         * @{var astronomy_photo = APICall.AstronomyPhotoOfTheDay();}
+         * <h3>@astronomy_photo.title</h3>
+         * <img src=@astronomy_photo.url>
+         * <small>@astronomy_photo.explanation</small> 
+         *
+         * There's also a date and hdurl variable attached to the APOD model*/
+        public static APOD AstronomyPhotoOfTheDay() 
+        {
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+            var client = new RestClient("https://api.nasa.gov/planetary/apod/");
+            var request = new RestRequest($"/?api_key={apiKey}", DataFormat.Json);
+            var response = client.Get(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                    throw new Exception("API call invalid");
+            }
+            var apod = JsonConvert.DeserializeObject<APOD>(response.Content);
+            return apod;
+        }
+    }
+}
---
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/Models/APOD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRoverBestGroup3._0.Models
+{
+    public class APOD
+    {
+        public string date;
+        public string explanation;
+        public string hdurl;
+        public string title;
+        public string url;
+
+        public APOD(string date, string explanation, string hdurl, string title, string url)
+        {
+            this.date = date;
+            this.explanation = explanation;
+            this.hdurl = hdurl;
+            this.title = title;
+            this.url = url; 
+        }
+    }
+}
