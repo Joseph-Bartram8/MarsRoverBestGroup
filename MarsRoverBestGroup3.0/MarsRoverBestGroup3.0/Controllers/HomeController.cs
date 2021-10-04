48a78e5351ba23226a10393815f063c8a08817f8
Claire Davies <C.Davies@ucas.ac.uk>
2021-10-04 16:29:40 +0100

    working method for date conversion?

--- MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/Controllers/HomeController.cs
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/Controllers/HomeController.cs
@@ -33,5 +33,7 @@ namespace MarsRoverBestGroup3._0.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
---
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/Utilities/MarsDate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MarsRoverBestGroup3._0.Models
+{
+
+    public class MarsDate
+    {
+        public double EarthToMarsDate(DateTime dateQuery)
+        {
+            
+            DateTime baseTime = new DateTime(1873, 12, 29, 00, 00, 0);
+            DateTime datequery = new DateTime(2021, 10, 04, 00, 00, 0);
+
+            System.TimeSpan diff1 = datequery.Subtract(baseTime);
+
+            Console.WriteLine(  diff1);
+            //create date time object  in 18?? 
+
+            //input date subract date of 18?? 
+            //Convert that to seconds
+            //feed seconds into formula
+            //put in a method and return the result
+            //DateTime theDate = DateTime.UtcNow;
+
+        //double MarsSolDate = (theDate - 2451561.5 ) / 1.02749125 + 44796.0;
+
+
+
+
+        }
+        
+        
+
+    }
+}
