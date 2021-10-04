6501d4ecbeb1f7b2b83d0bb63c6f655d4f0d6549
Ray <sewellray11@googlemail.com>
2021-10-04 16:24:29 +0100

    created generic api call method, removed comments, updated .gitignore

---
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/APICall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using RestSharp;
+using Newtonsoft.Json;
+using MarsRoverBestGroup3._0.Models;
+
+namespace MarsRoverBestGroup3._0
+{
+    public class APICall
+    {
+        public static IRestResponse GenericCall(string rest_request)
+        {
+            var apiKey = ConfigurationManager.AppSettings["APIKey"];
+            var client = new RestClient("https://api.nasa.gov/planetary/");
+            var request = new RestRequest($"{rest_request}/?api_key={apiKey}", DataFormat.Json);
+            var response = client.Get(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("API call invalid");
+            }
+            return response;
+        }
+        public static APOD AstronomyPhotoOfTheDay() 
+        {
+            var response = GenericCall("apod");
+            var apod = JsonConvert.DeserializeObject<APOD>(response.Content);
+            return apod;
+        }
+    }
+}
