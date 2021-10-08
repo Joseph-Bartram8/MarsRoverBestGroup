1df5750fadc6a04a25eb7302a77ae84ced60917a
Ray <sewellray11@googlemail.com>
2021-10-08 10:40:58 +0100

    Added date form to change gallery contents, created GalleryModel and PostDateModel

---
+++ MarsRoverBestGroup3.0/MarsRoverBestGroup3.0/Models/GalleryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRoverBestGroup3._0.Models
+{
+    public class GalleryModel
+    {
+        public List<Photo> photos { get; set; }
+        public DateTime date { get; set; }
+    }
+}
