e7463fe13cf3e50cdb1cba19627c1e8f709f448f
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-10-11 14:17:29 +0100

    fixed the methods and added tests

--- MarsRoverBestGroup3.0/MarsRoversTests/DateConverterTest.cs
+++ MarsRoverBestGroup3.0/MarsRoversTests/DateConverterTest.cs
@@ -19,5 +19,45 @@ namespace MarsRoversTests
 
             Assert.AreEqual(result, expectedSol);
         }
+        [Test]
+        public void PerseveranceSolTest()
+        {
+            DateTime testdate = new DateTime(2021, 10, 11);
+            int expectedSol = 228;
+
+            var result = DateConverter.PerseveranceSol(testdate);
+
+            Assert.AreEqual(result, expectedSol);
+        }
+        [Test]
+        public void OpportunitySolTest()
+        {
+            DateTime testdate = new DateTime(2021, 10, 11);
+            int expectedSol = 6296;
+
+            var result = DateConverter.OpportunitySol(testdate);
+
+            Assert.AreEqual(result, expectedSol);
+        }
+        [Test]
+        public void SpiritSolTest()
+        {
+            DateTime testdate = new DateTime(2021, 10, 11);
+            int expectedSol = 6316;
+
+            var result = DateConverter.SpiritSol(testdate);
+
+            Assert.AreEqual(result, expectedSol);
+        }
+        [Test]
+        public void SojournerSolTest()
+        {
+            DateTime testdate = new DateTime(2021, 10, 11);
+            int expectedSol = 8628;
+
+            var result = DateConverter.SojournerSol(testdate);
+
+            Assert.AreEqual(result, expectedSol);
+        }
     }
 }
