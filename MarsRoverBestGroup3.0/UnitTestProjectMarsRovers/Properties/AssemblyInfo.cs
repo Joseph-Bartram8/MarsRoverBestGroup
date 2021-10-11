b0033b1cc5172ac480a5c9266017b7941aa41975
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-10-11 10:43:11 +0100

    setting up tests

---
+++ MarsRoverBestGroup3.0/UnitTestProjectMarsRovers/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectMarsRovers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectMarsRovers")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ba72804-cb8f-4c47-86da-ac9ae4435c0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MarsRoverBestGroup3.0/UnitTestProjectMarsRovers/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MarsRoverBestGroup3._0.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace UnitTestProjectMarsRovers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestingPerseveranceSol()
+        {
+            var perseveranceSol
+        }
+       
+        }
+    }
+
